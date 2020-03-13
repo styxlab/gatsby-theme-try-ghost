3a657f2b680026beee754b584515d10bd78f17e8
cws <cws@localhost.localdomain>
2020-03-13 17:18:30 +0100

    only tracking

--- gatsby-theme-try-ghost/gatsby-config.js
+++ gatsby-theme-try-ghost/gatsby-config.js
@@ -15,7 +15,7 @@ const generateRSSFeed = require(`./src/utils/rss/generate-feed`)
 module.exports = (themeOptions) => {
     const siteConfig = themeOptions.siteConfig || siteConfigDefaults
     const ghostConfig = themeOptions.ghostConfig || ghostConfigDefaults
-    const downloadLocal = false || themeOptions.downloadLocal
+    //const downloadLocal = false || themeOptions.downloadLocal
 
     return {
         siteMetadata: siteConfig,
@@ -70,7 +70,7 @@ module.exports = (themeOptions) => {
                     ),
                     verbose: false,
                     // Option to disable this module (default: false)
-                    disable: !downloadLocal,
+                    disable: false,
                 },
             },
             {
