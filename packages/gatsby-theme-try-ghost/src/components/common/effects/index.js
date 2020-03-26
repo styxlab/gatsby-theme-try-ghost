a751ba380470d27010b8bc59ef5ae74233f37668
cws <cws@home.fritz.box>
2020-03-26 17:41:31 +0100

    this is cool!

--- packages/gatsby-theme-try-ghost/src/components/common/effects/index.js
+++ packages/gatsby-theme-try-ghost/src/components/common/effects/index.js
@@ -1,3 +1,2 @@
 export { default as HoverOnAvatar } from './HoverOnAvatar'
 export { default as StickyNavContainer } from './StickyNavContainer'
-export { default as DarkThemeProvider } from './DarkThemeProvider'
