f9e2e034cd8d537cf6deff894b4faaabafa485a8
cws <cws@home.fritz.box>
2020-04-09 11:54:33 +0200

    move query code

--- packages/gatsby-theme-try-ghost/gatsby-node.js
+++ packages/gatsby-theme-try-ghost/gatsby-node.js
@@ -3,6 +3,8 @@ const { paginate } = require(`gatsby-awesome-pagination`)
 const routing = require(`./src/utils/routing`)
 const fs = require(`fs`)
 
+const gatsbyNodeQuery = require(`./src/utils/gatsbyNodeQuery`)
+
 /**
  * Here is the place where Gatsby creates schema customizations.
  * This is needed to make the secondary_navigation field optional.
@@ -44,159 +46,7 @@ exports.createPages = async ({ graphql, actions }) => {
     /* Fragment are not yet possible here */
     /* Further info 👉🏼 https://github.com/gatsbyjs/gatsby/issues/12155 */
 
-    const result = await graphql(`
-        {
-            allGhostPost(sort: { order: DESC, fields: [published_at] }) {
-                edges {
-                    node {
-                        # Main fields
-                        id
-                        title
-                        slug
-                        url
-                        featured
-                        feature_image
-                        excerpt
-                        custom_excerpt
-                        visibility
-
-                        # Dates formatted
-                        created_at_pretty: created_at(formatString: "D MMM YYYY")
-                        published_at_pretty: published_at(formatString: "D MMM YYYY")
-                        updated_at_pretty: updated_at(formatString: "D MMM YYYY")
-
-                        # Dates unformatted
-                        created_at
-                        published_at
-                        updated_at
-
-                        # SEO
-                        meta_title
-                        meta_description
-                        og_description
-                        og_image
-                        og_title
-                        twitter_description
-                        twitter_image
-                        twitter_title
-
-                        # Authors
-                        authors {
-                            name
-                            slug
-                            url
-                            bio
-                            # email
-                            profile_image
-                            twitter
-                            facebook
-                            website
-                        }
-                        primary_author {
-                            name
-                            slug
-                            url
-                            bio
-                            # email
-                            profile_image
-                            twitter
-                            facebook
-                            website
-                        }
-
-                        # Tags
-                        primary_tag {
-                            name
-                            slug
-                            url
-                            description
-                            feature_image
-                            meta_description
-                            meta_title
-                            visibility
-                        }
-                        tags {
-                            name
-                            slug
-                            url
-                            description
-                            feature_image
-                            meta_description
-                            meta_title
-                            visibility
-                        }
-
-                        # Content
-                        plaintext
-                        html
-
-                        # Additional fields
-                        url
-                        canonical_url
-                        uuid
-                        page
-                        codeinjection_foot
-                        codeinjection_head
-                        codeinjection_styles
-                        comment_id
-                        reading_time
-
-                        # Transformed html
-                        children {
-                            ... on HtmlRehype {
-                                html
-                            }
-                        }
-
-                        # ImgSharp
-                        featureImageSharp {
-                            base
-                            childImageSharp {
-                                fluid(maxWidth: 1040) {
-                                    base64
-                                    aspectRatio
-                                    src
-                                    srcSet
-                                    sizes
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            allGhostTag(sort: { order: ASC, fields: name }) {
-                edges {
-                    node {
-                        slug
-                        url
-                        postCount
-                    }
-                }
-            }
-            allGhostAuthor(sort: { order: ASC, fields: name }) {
-                edges {
-                    node {
-                        slug
-                        url
-                        postCount
-                    }
-                }
-            }
-            allGhostPage(sort: { order: ASC, fields: published_at }) {
-                edges {
-                    node {
-                        slug
-                        url
-                    }
-                }
-            }
-            site {
-                siteMetadata {
-                    postsPerPage
-                }
-            }
-        }
-    `)
+    const result = await graphql(`${gatsbyNodeQuery}`)
 
     // Check for any errors
     if (result.errors) {
