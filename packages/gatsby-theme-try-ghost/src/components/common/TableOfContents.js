03c53479f6fe0b5c50b2b012798af61bfd939e85
cws <cws@home.fritz.box>
2020-04-14 15:33:17 +0200

    toc-box styling

--- packages/gatsby-theme-try-ghost/src/components/common/TableOfContents.js
+++ packages/gatsby-theme-try-ghost/src/components/common/TableOfContents.js
@@ -1,103 +1,159 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
-//import { jsx } from 'theme-ui'
+import styled from "styled-components"
+import { useActiveHash } from "./effects/use-active-hash"
 
-const createItems = (toc, url, depth, maxDepth) => {
-    return (
-        toc.map((head, index) => (
-            <li className="toc-item" key={location.pathname + (head.url || depth + `-` + index)}>
-                {head.id &&
-                    <Link className="toc-link" to={`${url}#${head.id}`}>{head.heading}</Link>
-                }
-                {head.items &&
-                    <ul className="toc-list">
-                        {createItems(head.items, url, depth + 1, maxDepth)}
-                    </ul>
-                }
-            </li>
-        ))
-    )
+/*
+ * styles passed into styled(Component) don't overwrite base styles #1816
+ * https://github.com/styled-components/styled-components/issues/1816
+*/
+const TocContainer = styled.aside`
+    order: 1;
+`
+const TocDiv = styled.div`
+    && {
+        position: sticky;
+        top: 120px;
+        min-width: 260px;
+        font-family: -apple-system, Liberation Sans, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+            Ubuntu, Cantarell, Open Sans, Helvetica Neue, sans-serif;
+        font-size: 1.4rem;
+        line-height: 1.5em;
+        padding: 0 0.8em;
+    }
+`
+const TocTitle = styled.h2`
+    && {
+        color: #78757a;
+        font-size: 1.2rem;
+        letter-spacing: 0.075em;
+        margin-top: 0rem;
+        margin-bottom: 2rem;
+        text-transform: uppercase;
+        font-weight: 700;
+    }
+`
+const TocList = styled.ul`
+    && {
+        overflow:hidden;
+        position:relative;
+        list-style: none;
+        margin:0;
+        padding:0;
+        margin-top: -1.6rem;
+        padding-top: 0.6rem;
+        padding-left: 0.9rem;
+    }
+`
+const TocListSub = styled.ul`
+    && {
+        margin-top: 0.5rem;
+        padding-left: 1.6rem;
+    }
+`
+const TocItem = styled.li`
+    && {
+        list-style: none;
+        margin-bottom: calc(1.5rem / 2);
+    }
+`
+const TocLink = styled(Link)`
+    && {
+        height: 100%;
+        box-shadow: none;
+        color: #78757a;
+        text-decoration: none;
+
+        &::before {
+            background-color: #EEE;
+            content:' ';
+            display: inline-block;
+            height: inherit;
+            left: 0;
+            position:absolute;
+            width: 2px;
+            margin-left: 1px;
+        }
+        &:focus{
+            &::before {
+                background-color:#54BC4B;
+            }
+        }
+    }
+`
+const getHeadingIds = (toc, traverseFullDepth = true, maxDepth, recursionDepth = 1) => {
+    const idList = []
+    const hashToId = str => str.slice(1)
+
+    if (toc) {
+        for (const item of toc) {
+            item.url && idList.push(hashToId(item.url))
+
+            // Only traverse sub-items if specified (they are not displayed in ToC)
+            // recursion depth should only go up to 6 headings deep and may come in as
+            // undefined if not set in the tableOfContentsDepth frontmatter field
+            if (item.items && traverseFullDepth && recursionDepth < (maxDepth || 6)) {
+                idList.push(
+                    ...getHeadingIds(item.items, true, maxDepth, recursionDepth + 1)
+                )
+            }
+        }
+    }
+    return idList
 }
 
-const TableOfContents = ({ toc, url }) => (
-    <aside className="toc-container">
-        <div className="toc">
-            <nav className="toc">
-                <h2>Table of Contents</h2>
-                <ul className="toc-list">
-                    {createItems(toc, url, 1, 5)}
-                </ul>
-            </nav>
-        </div>
-    </aside>
+const isUnderDepthLimit = (depth, maxDepth) => (maxDepth === null ? true : depth < maxDepth)
+
+const createItems = (toc, url, depth, maxDepth, activeHash) => (
+    toc.map((head, index) => {
+        const isActive = head.id === `${activeHash}`
+
+        return (
+            <TocItem key={location.pathname + (head.id || depth + `-` + index)}>
+                {head.id &&
+                    <TocLink
+                        getProps={({ href, location }) => (location && location.href && location.href.includes(href) ? {
+                            style: { color: `#3eb0ef`,
+                                borderBottom: `1px solid #3eb0ef`},
+                        } : null)}
+                        to={`${url}#${head.id}`}>{head.heading}</TocLink>
+                }
+                {head.items && isUnderDepthLimit(depth, maxDepth) &&
+                    <TocListSub>
+                        {createItems(head.items, url, depth + 1, maxDepth)}
+                    </TocListSub>
+                }
+            </TocItem>
+        )
+    })
 )
 
-//const isUnderDepthLimit = (depth, maxDepth) => {
-//    if (maxDepth === null) {
-//        // if no maxDepth is passed in, continue to render more items
-//        return true
-//    } else {
-//        return depth < maxDepth
-//    }
-//}
+const TableOfContents = ({ toc, url, maxDepth = 3 }) => {
+    const activeHash = useActiveHash(getHeadingIds(toc, true, maxDepth))
 
-//// depth and maxDepth are used to figure out how many bullets deep to render in the ToC sidebar, if no
-//// max depth is set via the tableOfContentsDepth field in the frontmatter, all headings will be rendered
-//const createItems = (toc, url, depth, maxDepth) => {
-//    return (
-//        toc.map((item, index) => (
-//            <li key={location.pathname + (item.url || depth + `-` + index)}>
-//                {item.id && (
-//                    <Link sx={{ "&&": {
-//                        color: `textMuted`,
-//                        border: 0,
-//                        transition: t => `all ${t.transition.speed.fast} ${t.transition.curve.default}`,
-//                        ":hover": {
-//                            color: `link.color`,
-//                            borderBottom: t => `1px solid ${t.colors.link.hoverBorder}`,
-//                        },
-//                    }}}
-//                    getProps={({ href, location }) => (location && location.href && location.href.includes(href) ? {
-//                        style: { color: `#F00`,
-//                            borderBottom: `1px solid #00F` },
-//                    } : null)}
-//                    to={`${url}#${item.id}`}>{item.heading}</Link>
-//                )}
-//                {item.items && isUnderDepthLimit(depth, maxDepth) && (
-//                    <ul sx={{ color: `textMuted`, listStyle: `none`, ml: 5 }}>
-//                        {createItems(item.items, url, depth + 1, maxDepth)}
-//                    </ul>
-//                )}
-//            </li>
-//        ))
-//    )
-//}
-//
-//const TableOfContents = ({ toc, url }) => {
-//    return toc.length > 0 ? (
-//        <nav sx={{
-//            mb: [8, null, null, null, null, 0],
-//            pb: [6, null, null, null, null, 0],
-//            borderBottom: t => [
-//                `1px solid #000`,
-//                null,null,null,null,0],
-//        }}>
-//            <h2 sx={{ color: `textMuted`,
-//                fontSize: 1, letterSpacing: `tracked`,
-//                mt: 0, textTransform: `uppercase` }}>Table of Contents</h2>
-//            <ul sx={{ listStyle: `none`, m: 0 }}>
-//                {createItems(toc, url, 1, 5)}
-//            </ul>
-//        </nav>
-//    ) : null
-//}
+    return (
+        <TocContainer>
+            <TocDiv>
+                <nav>
+                    <TocTitle>
+                        Table of Contents
+                    </TocTitle>
+                    <TocList>
+                        {createItems(toc, url, 1, maxDepth, activeHash)}
+                    </TocList>
+                </nav>
+            </TocDiv>
+        </TocContainer>
+    )
+}
 
 TableOfContents.propTypes = {
     toc: PropTypes.arrayOf(
         PropTypes.object,
     ).isRequired,
     url: PropTypes.string.isRequired,
+    depth: PropTypes.number,
 }
 
 export default TableOfContents
