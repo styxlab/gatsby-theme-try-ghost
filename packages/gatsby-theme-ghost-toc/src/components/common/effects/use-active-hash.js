8cdea90a62e92ddeb14ae524f680ce3d4043bf1c
cws <cws@home.fritz.box>
2020-04-20 23:30:35 +0200

    code cleanup

--- packages/gatsby-theme-ghost-toc/src/components/common/effects/use-active-hash.js
+++ packages/gatsby-theme-ghost-toc/src/components/common/effects/use-active-hash.js
@@ -19,13 +19,14 @@ export const useActiveHash = (itemIds, rootMargin = undefined) => {
         },
         { rootMargin: rootMargin || `0% 0% -90% 0%` })
 
-        itemIds
-            .filter(id => id !== `error-missing-id`)
-            .forEach(id => observer.observe(document.getElementById(id)))
+        const trigger = (id, key) => {
+            const element = document.getElementById(id)
+            element && typeof element === `object` ? observer[key](element) : null
+        }
 
-        return () => itemIds
-            .filter(id => id !== `error-missing-id`)
-            .forEach(id => observer.unobserve(document.getElementById(id)))
+        itemIds.map(id => trigger(id, `observe`))
+
+        return () => itemIds.map(id => trigger(id, `unobserve`))
     }, [])
 
     return activeHash
