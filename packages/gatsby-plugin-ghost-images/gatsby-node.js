b0da92d8152085a4c7217dae6256be41e7779ec3
cws <cws@home.fritz.box>
2020-05-18 10:42:50 +0200

    test

---
+++ packages/gatsby-plugin-ghost-images/gatsby-node.js
@@ -0,0 +1,87 @@
+"use strict";
+
+const _ = require(`lodash`);
+
+const {
+  createRemoteFileNode
+} = require(`gatsby-source-filesystem`);
+
+const ext = `_sharp`;
+const pluginDefaults = {
+  lookup: [],
+  exclude: () => false,
+  verbose: false,
+  disable: false
+};
+
+exports.onCreateNode = async function ({
+  node,
+  actions,
+  createNodeId,
+  reporter,
+  cache,
+  store
+}, pluginOptions) {
+  const {
+    createNode
+  } = actions;
+
+  const {
+    lookup,
+    exclude,
+    verbose,
+    disable
+  } = _.merge({}, pluginDefaults, pluginOptions); // leave if node is excluded by user
+
+
+  if (exclude(node) || disable) {
+    return {};
+  }
+
+  const imgNode = lookup.filter(item => item.type === node.internal.type); // leave if node type does not match
+
+  if (imgNode.length === 0) {
+    return {};
+  }
+
+  const allImgTags = imgNode[0].imgTags.filter(item => node[item] !== null && node[item] !== undefined); // leave if image field is empty
+
+  if (allImgTags.length === 0) {
+    return {};
+  } // remaining image fields
+
+
+  const promises = allImgTags.map(async tag => {
+    const imgUrl = node[tag];
+
+    if (verbose) {
+      reporter.info(`${node.internal.type}/${tag}/${node.slug}/${imgUrl}`);
+    }
+
+    return await createRemoteFileNode({
+      url: imgUrl,
+      parentNodeId: node.id,
+      createNode,
+      createNodeId,
+      cache,
+      store
+    });
+  });
+  let fileNodes;
+
+  try {
+    fileNodes = await Promise.all(promises);
+  } catch (err) {
+    reporter.panicOnBuild(`Error processing images ${node.absolutePath ? `file ${node.absolutePath}` : `in node ${node.id}`}:\n ${err}`);
+    return {};
+  }
+
+  fileNodes.map((fileNode, i) => {
+    const id = `${_.camelCase(`${allImgTags[i]}${ext}`)}___NODE`; //if (verbose) {
+    //    reporter.info(`${node.slug}/${allImgTags[i]}/${id}/${fileNode.id}`)
+    //}
+
+    node[id] = fileNode.id;
+  });
+  return {};
+};
