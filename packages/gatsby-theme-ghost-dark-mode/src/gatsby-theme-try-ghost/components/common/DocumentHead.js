63126819e8eb10b72960d05ceccce81d873117d0
cws <cws@home.fritz.box>
2020-06-03 10:45:27 +0200

    fix class

--- packages/gatsby-theme-ghost-dark-mode/src/gatsby-theme-try-ghost/components/common/DocumentHead.js
+++ packages/gatsby-theme-ghost-dark-mode/src/gatsby-theme-try-ghost/components/common/DocumentHead.js
@@ -7,9 +7,9 @@ import ThemeContext from '../../../context/ThemeContext'
 const DocumentHead = ({ site, className, action }) => (
     <ThemeContext.Consumer>{theme => (
         <Helmet>
-            <html lang={site.lang} class="casper"/>
+            <html lang={site.lang} className="casper"/>
             <style type="text/css">{`${site.codeinjection_styles}`}</style>
-            <body className={`${className} ${theme.dark && `dark`}  ${action === `subscribe` && `subscribe-success`}`} />
+            <body className={`${className} ${theme.dark && `dark`}  ${action === `subscribe` ? `subscribe-success` : ``}`} />
         </Helmet>
     )}
     </ThemeContext.Consumer>
