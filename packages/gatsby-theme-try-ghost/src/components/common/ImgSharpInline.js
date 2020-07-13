b89ed8e556c13b9b6eed9c3f0132589e8334cd5d
cws <cws@home.fritz.box>
2020-07-14 00:13:20 +0200

    fix image glitches

--- packages/gatsby-theme-try-ghost/src/components/common/ImgSharpInline.js
+++ packages/gatsby-theme-try-ghost/src/components/common/ImgSharpInline.js
@@ -2,20 +2,22 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Img from 'gatsby-image'
 
-const ImgSharpInline = ({ parentClassName, className, fluidImg, alt, maxWidth = 0 }) => {
+const ImgSharpInline = ({ parentClassName, className, fluidImg, alt, maxWidth }) => {
     const classList = parentClassName && parentClassName.split(` `) || []
     const fullWidth = classList.includes(`kg-width-full`)
     const bookmark = classList.includes(`kg-bookmark-thumbnail`)
+    const image = classList.includes(`kg-image-card`)
 
     const fluid = fluidImg && JSON.parse(fluidImg)
-    const mWidth = !fullWidth && maxWidth > 0 && parseInt(maxWidth, 10)
+    const mWidth = !fullWidth && image && parseInt(maxWidth, 10) > 0 && maxWidth
+    const fWidth = parseInt(maxWidth, 10) > 0 && Math.min(parseInt(maxWidth, 10), 6000) || 6000
 
     return (
         <Img
             style={{
                 height: `100%`,
                 position: `${bookmark ? null : `relative`}`,
-                width: `${fullWidth ? `6000px` : null}`,
+                width: `${fullWidth ? `${fWidth}px` : null}`,
                 maxWidth: `${mWidth ? `${mWidth}px` : null}`,
                 margin: `${mWidth ? `auto` : null}`,
             }}
@@ -32,7 +34,7 @@ ImgSharpInline.propTypes = {
     className: PropTypes.string,
     fluidImg: PropTypes.string.isRequired,
     alt: PropTypes.string,
-    maxWidth: PropTypes.number,
+    maxWidth: PropTypes.string,
 }
 
 export default ImgSharpInline
