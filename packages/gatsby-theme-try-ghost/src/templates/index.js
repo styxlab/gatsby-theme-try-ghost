377bce35175f471fc704d5d8071adc202379702f
marcoSven <me@marcosven.com>
2020-11-04 08:17:07 -0800

    only pass parsedQuery as props if type object

--- packages/gatsby-theme-try-ghost/src/templates/index.js
+++ packages/gatsby-theme-try-ghost/src/templates/index.js
@@ -18,14 +18,13 @@ import { GlobalStateContext } from "../context/GlobalState"
 const Index = ({ data, location, pageContext }) => {
     const posts = data.allGhostPost.edges
     const parsedQuery = location.search && location.search.length > 0 && queryString.parse(location.search)
-
     return (
         <GlobalStateContext.Consumer>{ g => (
             <React.Fragment>
                 <MetaData location={location} image={data.file} />
                 <StickyNavContainer throttle={300} activeClass="fixed-nav-active" render={ sticky => (
                     <OverlayContainer render={ overlay => (
-                        <Layout parsedQuery={parsedQuery} isHome={true} header={<HeaderIndex overlay={overlay}/>} sticky={sticky} overlay={overlay} >
+                        <Layout parsedQuery={parsedQuery !== '' ? parsedQuery : undefined } isHome={true} header={<HeaderIndex overlay={overlay}/>} sticky={sticky} overlay={overlay} >
                             <PostView globalState={g} pageContext={pageContext} posts={posts} isHome={true} />
                         </Layout>
                     )}/>
