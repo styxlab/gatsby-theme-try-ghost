96437cc67e8f4433abf721f18498de3fa77444e8
marcoSven <me@marcosven.com>
2020-11-06 19:45:51 -0800

    Fade in close button

--- packages/gatsby-theme-ghost-members/src/gatsby-theme-try-ghost/components/common/SubscribeSuccess.js
+++ packages/gatsby-theme-ghost-members/src/gatsby-theme-try-ghost/components/common/SubscribeSuccess.js
@@ -6,21 +6,28 @@ import { useLang, get } from '../../../utils/use-lang'
 const SubscribeSuccess = ({ parsedQuery = {}, title }) => {
     const [type, setType] = useState(``)
     const [closeState, setCloseState] = useState(``)
+    const [closeOpacity, setCloseOpacity] = useState(0)
     const { action, success } = parsedQuery
     const ssr = typeof window === `undefined`
     const showBanner = ssr || (action && action === `subscribe` && success !== undefined)
     const text = get(useLang())
     const message = success === `true` ? `${text(`SUBSCRIBED_TO`)} ${title}!` : `Could not sign up! Invalid sign up link.`
     useEffect(() => {
+        const timer = setTimeout(
+            () =>
+                setCloseOpacity(1),
+            1500
+        );
         setType(
             success === `true` ? `success` : `failure`
         )
+        return () => clearTimeout(timer);
     }
     , [setType, action])
 
     return (
         <div className={`subscribe-notification subscribe-${type}-message${closeState}`} style={{ opacity: showBanner }}>
-            <a
+            <a style={{opacity: closeOpacity }}
                 onClick={(e) => {
                     e.preventDefault()
                     setCloseState(` close`)
