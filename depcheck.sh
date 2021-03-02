6dec71bbca08b4a8c5f9e5fa74803a92d46054b7
jj <jj>
2021-03-03 00:20:07 +0100

    stage13

--- depcheck.sh
+++ depcheck.sh
@@ -4,40 +4,40 @@ npm-upgrade
 
 
 cd packages/gatsby-source-try-ghost
-#npm-upgrade
+npm-upgrade
 
-#cd ../gatsby-plugin-ghost-images
-#npm-upgrade
+cd ../gatsby-plugin-ghost-images
+npm-upgrade
 
-#cd ../gatsby-rehype-ghost-links
-#npm-upgrade
+cd ../gatsby-rehype-ghost-links
+npm-upgrade
 
-#cd ../gatsby-rehype-inline-images
-#npm-upgrade
+cd ../gatsby-rehype-inline-images
+npm-upgrade
 
-#cd ../gatsby-rehype-prismjs
-#npm-upgrade
+cd ../gatsby-rehype-prismjs
+npm-upgrade
 
-#cd ../gatsby-theme-ghost-amp
-#npm-upgrade
+cd ../gatsby-theme-ghost-amp
+npm-upgrade
 
-#cd ../gatsby-theme-ghost-commento
-#npm-upgrade
+cd ../gatsby-theme-ghost-commento
+npm-upgrade
 
-#cd ../gatsby-theme-ghost-contact
-#npm-upgrade
+cd ../gatsby-theme-ghost-contact
+npm-upgrade
 
-#cd ../gatsby-theme-ghost-dark-mode
-#npm-upgrade
+cd ../gatsby-theme-ghost-dark-mode
+npm-upgrade
 
-#cd ../gatsby-theme-ghost-members
-#npm-upgrade
+cd ../gatsby-theme-ghost-members
+npm-upgrade
 
-#cd ../gatsby-theme-ghost-toc
-#npm-upgrade
+cd ../gatsby-theme-ghost-toc
+npm-upgrade
 
-#cd ../gatsby-transformer-rehype
-#npm-upgrade
+cd ../gatsby-transformer-rehype
+npm-upgrade
 
 cd ../gatsby-theme-try-ghost
 npm-upgrade
