f1ebfbaf33f4702f1f719dccce51d0cfe809bce3
Shaun Wilde <shaun.wilde@estimateone.com>
2021-08-28 18:30:25 +1000

    feat: switch to using fontawesome and faTags

--- packages/gatsby-theme-ghost-tags/src/gatsby-theme-try-ghost/components/common/ShowTags.js
+++ packages/gatsby-theme-ghost-tags/src/gatsby-theme-try-ghost/components/common/ShowTags.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import TagIcon from '../../../components/common/icons/tag-icon'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTags } from '@fortawesome/free-solid-svg-icons'
 
 const ShowTags = ({data}) => {
     const tags = data.allTagsPage.edges[0].node
     return (
         <React.Fragment>
-            <a href={tags.url} className="social-link" title={tags.title}><TagIcon /></a>
+            <a href={tags.url} className="social-link" title={tags.title}><FontAwesomeIcon icon={faTags} inverse /></a>
         </React.Fragment>
     )
 }
